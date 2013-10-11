2e435791566afc6c407e50b357c0f01e006d0505
Carkci <Carkci@macbook.local>
2013-10-11 14:04:42 +0200

    Initial Import

---
+++ Mobile.IOS/Mobile_IOSViewController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Mobile.IOS
+{
+	public partial class Mobile_IOSViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public Mobile_IOSViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "Mobile_IOSViewController_iPhone" : "Mobile_IOSViewController_iPad", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
---
+++ Mobile.IOS/Mobile_IOSViewController.designer.cs
@@ -0,0 +1,17 @@
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace Mobile.IOS
+{
+	[Register ("Mobile_IOSViewController")]
+	partial class Mobile_IOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
